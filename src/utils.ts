108eec3cb5d47f3a27d5dc72ece777329d949c7c
Huakun Shen <huakun.shen@huakunshen.com>
2024-10-15 22:07:35 -0400

    Got windows supported

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,5 @@
 import path from "node:path";
+import { fileURLToPath } from 'node:url';
 
 export function generateUUID(): string {
   return new Array(4)
@@ -8,7 +9,8 @@ export function generateUUID(): string {
 }
 
 export function getProjectRoot(): string {
-  const fileUrl = new URL(import.meta.url).pathname;
-  const folderPath = path.dirname(path.dirname(fileUrl));
+  // const fileUrl = new URL(import.meta.url).pathname;
+  const __filename = fileURLToPath(import.meta.url);
+  const folderPath = path.dirname(path.dirname(__filename));
   return folderPath;
 }
